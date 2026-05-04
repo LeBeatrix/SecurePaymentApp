4f71b8aeb5e78d90319cb936394c46d32e8b51a1
Lené Prinsloo <prinsloolene@gmail.com>
2026-05-05 00:56:10 +0200

    Fix Payment and build error

--- SecureAPI/Models/Payment.cs
+++ SecureAPI/Models/Payment.cs
@@ -3,6 +3,7 @@ namespace SecureAPI.Models
     public class Payment
     {
         public int Id { get; set; }
+        public required string AccountNumber { get; set; }
         public decimal Amount { get; set; }
         public required string Currency { get; set; }
         public required string SwiftCode { get; set; }
